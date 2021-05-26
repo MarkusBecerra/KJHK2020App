e2783b78d0ec0b51ea0ee22cebde33d766f128d2
Markus Becerra <markusbecerra@gmail.com>
2021-05-26 14:37:18 -0500

    changes

--- KJHK/src/components/ContactContainer.tsx
+++ KJHK/src/components/ContactContainer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IonGrid, IonRow, IonCol, IonContent } from '@ionic/react';
+import { IonGrid, IonRow, IonCol, IonContent, IonButton, IonButtons, IonImg } from '@ionic/react';
 import './ContactContainer.css';
 
 
@@ -10,40 +10,90 @@ const ContactContainer: React.FC = () => {
  
     <IonGrid>
         <IonRow>
-            <div>
-                 {/* class:contact-label */}
+            <IonCol>
+                      <div className="contact-label">
+                          {/* class:contact-label */}
                 REQUEST LINE:
             </div>
 
-                  <a href="tel:+1-785-864-4747" target="_blank" rel="noopener noreferrer">
-                <div>
-                    {/* class: request-line-link */}
-                    <div>785-864-4747</div>
-                    {/* class="request-line-text" */}
-                </div>
-            </a>
+                      <a className="requestline-link" href="tel:+1-785-864-4747" target="_blank" rel="noopener noreferrer">
+                          <img className = "requestline" src="../assets/04_RequestLine/SVG/RequestLineBubble.svg" alt="../assets/icon.png" />
+                      </a>
+            </IonCol>
+
         </IonRow>
 
         <IonRow>
             <IonCol>
-                <div>
+                <div className="contact-label">
                           {/* class:contact-label */}
                     CONNECT WITH US:
                 </div>
 
-                <div>
+                <div className="social-media-table">
                           {/* class:socal-media-table */}
-                          <a href="https://www.facebook.com/KJHK907/" target="_blank" rel="noopener noreferrer">
-                         <img src="../assets/img/Facebook_DS.svg" alt="../assets/icon.png" />
-                     </a>
+                          <IonButtons slot="end">
+                              <IonButton style={{
+                                  color: "white",
+                                  borderRadius: "100%",
+                                  width: 150,
+                                  height: 150
+                              }} href="https://www.facebook.com/KJHK907/" target="_blank" rel="noopener noreferrer" shape="round">
+                                  <IonImg style={{
+                                      width: 5000,
+                                      height: 500
+                                  }} src="../assets/04_RequestLine/SVG/Facebook.svg"/>
+                              </IonButton>
 
-                          <a href=" https://twitter.com/kjhk" target="_blank" rel="noopener noreferrer">
-                              <img src="../assets/img/Twitter_DS.svg" alt="../assets/icon.png" />
-                     </a>
+                              <IonButton style={{
+                                  color: "white",
+                                  borderRadius: "100%",
+                                  width: 150,
+                                  height: 150
+                              }} href="https://twitter.com/kjhk" target="_blank" rel="noopener noreferrer" shape="round">
+                                  <IonImg style={{
+                                      width: 5000,
+                                      height: 500
+                                  }} src="../assets/04_RequestLine/SVG/Twitter.svg" />
+                              </IonButton>
 
-                          <a href=" https://www.instagram.com/kjhk90_7" target="_blank" rel="noopener noreferrer">
-                              <img src="../assets/img/Instagram_DS2.svg" alt="../assets/icon.png" />
-                     </a>
+                              <IonButton style={{
+                                  color: "white",
+                                  borderRadius: "100%",
+                                  width: 150,
+                                  height: 150
+                              }} href="https://www.instagram.com/kjhk90_7" target="_blank" rel="noopener noreferrer" shape="round">
+                                  <IonImg style={{
+                                      width: 5000,
+                                      height: 500
+                                  }} src="../assets/04_RequestLine/SVG/Instagram.svg" />
+                              </IonButton>
+                          </IonButtons>
+                                           <IonButtons slot="end">
+                          <IonButton style={{
+                              color: "white",
+                              borderRadius: "100%",
+                              width: 150,
+                              height: 150
+                          }} href="http://kjhk.org/web/" target="_blank" rel="noopener noreferrer" shape="round">
+                              <IonImg style={{
+                                  width: 5000,
+                                  height: 500
+                              }} src="../assets/04_RequestLine/SVG/Website.svg" />
+                          </IonButton>
+
+                          <IonButton style={{
+                              color: "white",
+                              borderRadius: "100%",
+                              width: 150,
+                              height: 150
+                          }} href="https://www.youtube.com/channel/UCqwb8_dzDRSE5RyrgP0q-tA" target="_blank" rel="noopener noreferrer" shape="round">
+                              <IonImg style={{
+                                  width: 5000,
+                                  height: 500
+                              }} src="../assets/04_RequestLine/SVG/Youtube.svg" />
+                          </IonButton>
+                      </IonButtons>
                 </div>
 
             </IonCol>
@@ -51,14 +101,8 @@ const ContactContainer: React.FC = () => {
 
         <IonRow>
             <IonCol>
-            
-            <a href="http://kjhk.org/web/" target="_blank" rel="noopener noreferrer"> 
-                    <img src="../assets/img/kjhk-webs_DS.svg" alt="../assets/icon.png"/>
-            </a>
 
-            <a href="https://www.youtube.com/channel/UCqwb8_dzDRSE5RyrgP0q-tA" target="_blank" rel="noopener noreferrer"> 
-                    <img src="../assets/img/Youtube_DS.svg" alt="../assets/icon.png"/>
-            </a>
+
 
             </IonCol>
         </IonRow>
